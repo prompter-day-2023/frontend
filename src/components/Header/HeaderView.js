9244a92c10f7c13caa5ab94113525b6494556231
kimgosung <rlarhtjd0123@gmail.com>
2023-08-28 11:59:56 +0900

    ♻️Refactor: 헤더 부분 수정

--- src/components/Header/HeaderView.js
+++ src/components/Header/HeaderView.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import LogoImage from '../../assets/Images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 const HeaderView = () => {
   const [currentPage, setCurrentPage] = useState("home"); // 기본 페이지를 "home"으로 설정
-
   return (
     <HeaderStyled>
       <Image src={LogoImage} alt="Logo" />
       <ButtonWrapper>
-        <HomeButton onClick={() => setCurrentPage("home")} isActive={currentPage === "home"}>
+        <StyledLink to="/" onClick={() => setCurrentPage("home")} isActive={currentPage === "home"}>
           꾸미
           {currentPage === "home" && <Underline />}
-        </HomeButton>
+        </StyledLink>
       </ButtonWrapper>
       <ButtonWrapper>
-        <InfoButton onClick={() => setCurrentPage("info")} isActive={currentPage === "info"}>
+        <StyledLink to="/info" onClick={() => setCurrentPage("info")} isActive={currentPage === "info"}>
           활용법
           {currentPage === "info" && <Underline />}
-        </InfoButton>
+        </StyledLink>
       </ButtonWrapper>
     </HeaderStyled>
   );
@@ -50,7 +50,7 @@ const ButtonWrapper = styled.div`
   margin-left: 15px;
 `;
 
-const HomeButton = styled.button`
+const StyledLink = styled(Link)`
   display: flex;
   justify-content: center;
   align-items:center;
@@ -59,8 +59,9 @@ const HomeButton = styled.button`
   font-family: "NanumSquareRound";
   font-size: 20px;
   cursor: pointer;
-  padding: 15px;
-  border: none;
+  padding-left: 10px;
+  padding-right: 10px;
+  text-decoration: none;
   background-color: #fff;
 
   &:hover {
@@ -80,41 +81,6 @@ const HomeButton = styled.button`
       background-color: #242424;
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
-    }
-  `}
-`;
-
-const InfoButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items:center;
-  height: 100%;
-  color: #242424;
-  font-family: "NanumSquareRound";
-  font-size: 20px;
-  cursor: pointer;
-  padding: 20px;
-  border: none;
-  background-color: #fff;
-
-  &:hover {
-    color: #4192F7;
-  }
-
-  ${props =>
-    props.isActive &&
-    `
-    &:after {
-      content: "";
-      position: absolute;
-      bottom: 0px;
-      left: 0;
-      width: 100%;
-      height: 5px;
-      background-color: #242424;
-      border-top-left-radius: 10px;
-      border-top-right-radius: 10px;
-
     }
   `}
 `;
